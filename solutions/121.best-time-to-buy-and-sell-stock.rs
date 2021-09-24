7aff81649827f36cd3cd055cd740326d55f5ca82
Bryan Lee <doslby@gmail.com>
2021-09-24 22:49:53 +0800

    feat: ✨ add new solution of best-time-to-buy-and-sell-stock

--- solutions/121.best-time-to-buy-and-sell-stock.rs
+++ solutions/121.best-time-to-buy-and-sell-stock.rs
@@ -10,10 +10,15 @@ impl Solution {
         use std::cmp::{max, min};
         let mut max_profit = 0;
         let mut min_seen = std::i32::MAX;
-        prices.into_iter().for_each(|value| {
-            max_profit = max(max_profit, value - min_seen);
-            min_seen = min(min_seen, value);
-        });
+        // prices.into_iter().for_each(|value| {
+        //     max_profit = max(max_profit, value - min_seen);
+        //     min_seen = min(min_seen, value);
+        // });
+
+        for price in prices {
+            min_seen = min(min_seen, price);
+            max_profit = max(max_profit, price - min_seen);
+        }
         max_profit
     }
 }
