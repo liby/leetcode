0f0d0b1e08fdb9873b991f462267fd94c9c2c670
Bryan Lee <doslby@gmail.com>
2022-03-15 00:59:17 +0800

    feat: ✨ optimizes the code logic for auto commit

--- src/main.rs
+++ src/main.rs
@@ -36,33 +36,47 @@ fn main() -> Result<(), Error> {
     let mut output = File::create(path)?;
     write!(output, "# Rust Solution\n\n")?;
 
+    let mut commit_message_vec = vec![String::from("feat: ✨ add new solutions")];
     let files_len = filename_vec.len();
-    for i in 0..files_len {
-        let format_path = format!("solutions/{}", filename_vec[i].to_str().unwrap());
-        let file = BufReader::new(File::open(format_path)?);
-        let title_line = file
-            .lines()
-            .map(|line| line.unwrap())
-            .skip(3)
-            .next()
-            .unwrap();
+    for filename in filename_vec.iter().take(files_len) {
+        let format_path = format!("solutions/{}", filename.to_str().unwrap());
+        let file = BufReader::new(File::open(format_path.clone())?);
+        let title_line = file.lines().map(|line| line.unwrap()).nth(3).unwrap();
 
         let format_filename = format!(
             "{}. [{}](https://github.com/liby/leetcode/blob/main/solutions/{})",
-            conversion_filename_vec(&filename_vec[i])[0],
+            conversion_filename_vec(filename)[0],
             conversion_title_vec(&title_line)[2..].join(" "),
-            filename_vec[i].to_str().unwrap(),
+            filename.to_str().unwrap(),
         );
-        write!(output, "{}\n", format_filename)?;
+        writeln!(output, "{}", format_filename)?;
+
+        let metadata = fs::metadata(format_path).unwrap();
+        let creation_time = metadata.created()?;
+        if creation_time.elapsed().unwrap().as_secs() < (2 * 3600) && metadata.is_file() {
+            commit_message_vec.push(format!(
+                "feat: ✨ add new solution of {}",
+                conversion_filename_vec(filename)[1]
+            ));
+        }
     }
 
-    let commit_message = format!(
-        "feat: ✨ add new solution of {}",
-        conversion_filename_vec(&filename_vec[files_len - 1])[1]
-    );
-
-    Command::new("git").arg("add").arg(".").output().expect("shell exec error!");
-    Command::new("git").arg("commit").arg("-m").arg(&commit_message).output().expect("shell exec error!");
+    let commit_message = if commit_message_vec.len() > 2 {
+        &commit_message_vec[0]
+    } else {
+        &commit_message_vec[1]
+    };
+    Command::new("git")
+        .arg("add")
+        .arg("--all")
+        .output()
+        .expect("shell exec error!");
+    Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(commit_message)
+        .output()
+        .expect("shell exec error!");
 
     Ok(())
 }
